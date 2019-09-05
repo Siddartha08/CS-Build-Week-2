c41703ceb6633629ac6c5de1ab17e810aeb05467
Kyle Baker <tokylebaker@gmail.com>
2019-09-04 17:56:00 -0700

    added other button directions

--- treasure-hunter/src/app/ActionButtons/ActionButtons.js
+++ treasure-hunter/src/app/ActionButtons/ActionButtons.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react'
-import { FlexRow } from 'style'
-import {
-  Button
-} from "@material-ui/core";
-import withData from './data/withData'
-
+import React, { Component } from "react";
+import { FlexRow } from "style";
+import { Button } from "@material-ui/core";
+import withData from "./data/withData";
 
 class ActionButtons extends Component {
   move = dir => {
     // TODO look up room id in dictionary to see if we know the next room id and send that along if so
 
-    this.props.move(dir).then(room => {
-      console.log(room)
-    }).catch(err => {})
+    this.props
+      .move(dir)
+      .then(room => {
+        console.log(room);
+      })
+      .catch(err => {});
   };
 
   render() {
     return (
       <FlexRow alignCenter>
-        <Button onClick={() => this.move("n")}>N</Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => this.move("n")}
+        >
+          N
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => this.move("s")}
+        >
+          S
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => this.move("w")}
+        >
+          W
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => this.move("e")}
+        >
+          E
+        </Button>
       </FlexRow>
-    )
+    );
   }
-
 }
 
-export default withData(ActionButtons)
+export default withData(ActionButtons);
